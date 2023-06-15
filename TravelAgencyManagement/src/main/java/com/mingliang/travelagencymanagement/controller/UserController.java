9c4f67b8dffdbf690cda2099380fddcbf0be7b36
洮羱芝闇 <83948628+tyza66@users.noreply.github.com>
2023-06-15 10:23:02 +0800

    准备工作

---
+++ TravelAgencyManagement/src/main/java/com/mingliang/travelagencymanagement/controller/UserController.java
@@ -0,0 +1,10 @@
+package com.mingliang.travelagencymanagement.controller;
+
+/**
+ * Author: tyza66
+ * Date: 2023/6/15 10:13
+ * Github: https://github.com/tyza66
+ **/
+
+public class UserController {
+}
