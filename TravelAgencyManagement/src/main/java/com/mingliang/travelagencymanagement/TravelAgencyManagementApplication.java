1a4e2606966c21f4395d4fec68f88cba8002b600
洮羱芝闇 <83948628+tyza66@users.noreply.github.com>
2023-06-15 09:39:55 +0800

    浅改

---
+++ TravelAgencyManagement/src/main/java/com/mingliang/travelagencymanagement/TravelAgencyManagementApplication.java
@@ -0,0 +1,13 @@
+package com.mingliang.travelagencymanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelAgencyManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelAgencyManagementApplication.class, args);
+    }
+
+}
