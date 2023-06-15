ef03490d235323e6ac61f0981b07f4302e3fbb7c
洮羱芝闇 <83948628+tyza66@users.noreply.github.com>
2023-06-15 11:10:30 +0800

    修改了一些配置

---
+++ TravelAgencyManagement/src/main/java/com/mingliang/travelagencymanagement/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package com.mingliang.travelagencymanagement.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.mingliang.travelagencymanagement.entity.User;
+
+/**
+ * Author: tyza66
+ * Date: 2023/6/15 10:46
+ * Github: https://github.com/tyza66
+ **/
+
+public interface UserMapper extends BaseMapper<User> {
+}
---
+++ TravelAgencyManagement/src/main/java/com/mingliang/travelagencymanagement/service/UserService.java
@@ -0,0 +1,10 @@
+package com.mingliang.travelagencymanagement.service;
+
+/**
+ * Author: tyza66
+ * Date: 2023/6/15 10:50
+ * Github: https://github.com/tyza66
+ **/
+
+public interface UserService {
+}
