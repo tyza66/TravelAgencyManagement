a3ab5e89b5b49fee7c6574d27ad2237718f60b09
Li Bling <105184043+morsite@users.noreply.github.com>
2023-06-16 12:46:44 +0800

    1

--- TravelAgencyManagement/src/main/java/com/mingliang/travelagencymanagement/mapper/BuserMapper.java
+++ TravelAgencyManagement/src/main/java/com/mingliang/travelagencymanagement/mapper/BuserMapper.java
@@ -2,7 +2,12 @@ package com.mingliang.travelagencymanagement.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.mingliang.travelagencymanagement.entity.Buser;
+import org.apache.ibatis.annotations.Select;
+
+import java.sql.Timestamp;
+import java.util.List;
 
 public interface BuserMapper extends BaseMapper<Buser> {
-
+    @Select("select * from (SELECT o.out from `out` o,buser b where b.bid=o.bid  and b.bid=#{bid} ORDER BY o.out) e where e.out>SYSDATE()")
+    public Timestamp selectOut(int bid);
 }
--- TravelAgencyManagement/src/main/java/com/mingliang/travelagencymanagement/service/BuserService.java
+++ TravelAgencyManagement/src/main/java/com/mingliang/travelagencymanagement/service/BuserService.java
@@ -3,8 +3,10 @@ package com.mingliang.travelagencymanagement.service;
 
 import com.mingliang.travelagencymanagement.entity.Buser;
 
+import java.sql.Timestamp;
 import java.util.List;
 
 public interface BuserService {
     public List<Buser> selectAllBuser();
+    public Timestamp recentTime(int id);
 }
