4481e644be724e98db296192f1e21b8dcbd11631
Li Bling <105184043+morsite@users.noreply.github.com>
2023-06-16 12:53:58 +0800

    1

--- TravelAgencyManagement/src/main/java/com/mingliang/travelagencymanagement/service/impl/BuserServiceImpl.java
+++ TravelAgencyManagement/src/main/java/com/mingliang/travelagencymanagement/service/impl/BuserServiceImpl.java
@@ -6,14 +6,22 @@ import com.mingliang.travelagencymanagement.mapper.BuserMapper;
 import com.mingliang.travelagencymanagement.service.BuserService;
 import org.springframework.stereotype.Service;
 
+import java.sql.Timestamp;
+import java.util.Date;
 import java.util.List;
 
 
 @Service
 public class BuserServiceImpl extends ServiceImpl<BuserMapper, Buser> implements BuserService {
+    BuserMapper buserMapper;
     @Override
     public List<Buser> selectAllBuser() {
        return baseMapper.selectList(null);
     }
 
+    @Override
+    public Timestamp recentTime(int id) {
+        Timestamp timestamp = buserMapper.selectOut(id);
+        return timestamp;
+    }
 }
