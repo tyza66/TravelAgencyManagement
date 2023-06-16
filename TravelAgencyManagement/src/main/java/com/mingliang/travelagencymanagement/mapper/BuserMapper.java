d8c53ea4b1c34bfc9a198f05c565813c01a5ef38
Li Bling <105184043+morsite@users.noreply.github.com>
2023-06-16 14:25:45 +0800

    1

--- TravelAgencyManagement/src/main/java/com/mingliang/travelagencymanagement/mapper/BuserMapper.java
+++ TravelAgencyManagement/src/main/java/com/mingliang/travelagencymanagement/mapper/BuserMapper.java
@@ -8,8 +8,8 @@ import java.sql.Timestamp;
 import java.util.List;
 
 public interface BuserMapper extends BaseMapper<Buser> {
-    @Select("select * from (SELECT o.out from `out` o,buser b where b.bid=o.bid  and b.bid=#{bid} ORDER BY o.out) e where e.out>SYSDATE()")
-    public Timestamp selectOut(int bid);
+    @Select("select * from (SELECT o.out1 from out1 o,buser b where b.bid=o.bid  and b.bid=#{bid} ORDER BY o.out1) e where e.out1>SYSDATE()")
+    public Timestamp selectOut(String bid);
     @Select("select m.massage from massage m,out1 o,buser b where b.bid=#{bid} and b.bid=o.bid and o.oid=m.oid")
-    public String selectMassage(int bid);
+    public String selectMassage(String bid);
 }
