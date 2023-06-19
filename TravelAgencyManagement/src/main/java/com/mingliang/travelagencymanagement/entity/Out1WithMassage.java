01ae26bce57c7218e9153412850436e18af6fef3
洮羱芝闇 <83948628+tyza66@users.noreply.github.com>
2023-06-19 08:32:22 +0800

    多加一个实体类

---
+++ TravelAgencyManagement/src/main/java/com/mingliang/travelagencymanagement/entity/Out1WithMassage.java
@@ -0,0 +1,35 @@
+package com.mingliang.travelagencymanagement.entity;
+
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Timestamp;
+
+/**
+ * Author: tyza66
+ * Date: 2023/6/19 8:27
+ * Github: https://github.com/tyza66
+ **/
+
+@Data
+@NoArgsConstructor
+public class Out1WithMassage {
+    Integer oid;
+    String bid;
+    Integer gid;
+    Timestamp out1;
+    Timestamp back;
+    Integer massage_id;
+    String massage;
+
+    public Out1WithMassage(Out1 out1,Massage massage){
+        this.oid = out1.getOid();
+        this.bid = out1.getBid();
+        this.gid = out1.getGid();
+        this.out1 = out1.getOut1();
+        this.back = out1.getBack();
+        this.massage_id = massage.getMassage_id();
+        this.massage = massage.getMassage();
+    }
+}
